44c1f684e3649299992bd3b65effd8e2194be509
ClaireGillot <gillot.clr@gmail.com>
2021-08-20 15:54:50 +0200

    fixed error with cors

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const formidableMiddleware = require("express-formidable");
 const mongoose = require("mongoose");
 require("dotenv").config();
+const cors = require("cors");
 
 const app = express();
 
